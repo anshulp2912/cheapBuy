cb95368adb20fa6beea27ee40aeb691d27024c73
rohan22shah <42119767+rohan22shah@users.noreply.github.com>
2021-11-04 00:58:07 -0400

    Test Cases added
    Test Cases added for:
    Fetching Description of Walmart
    Fetching Description of Ebay
    Fetching Description of Costco
    URL Shortener

---
+++ test/test_url_shortener.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 00:27:27 2021
+
+@author: Rohan Shah
+"""
+
+from url_shortener import shorten_url
+
+def test_url_shortener():
+    url = 'https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038'
+    s_url = shorten_url(url)
+    assert 'tinyurl.com' in s_url
