c868158e4c8138634a88232ea1a48166c961357d
Anshul Patel <36321866+anshulp2912@users.noreply.github.com>
2021-11-04 01:20:43 -0400

    Added MultiThreading
    Implemented Multithreading to improve performance

--- code/web_scrappers/WebScrapper_Ebay.py
+++ code/web_scrappers/WebScrapper_Ebay.py
@@ -6,13 +6,47 @@ This code is licensed under MIT license (see LICENSE.MD for details)
 """
 
 from bs4 import BeautifulSoup
+from threading import Thread
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-class WebScrapper_Ebay():
+import sys
+sys.path.append('../')
+from utils.url_shortener import shorten_url
+
+class WebScrapper_Ebay(Thread):
     
-    def __init__(self,driver,description):
-        self.driver = driver
+    def __init__(self,description):
+        self.driver = self.get_driver()
         self.description = description
+        self.result = {}
+        super(WebScrapper_Ebay,self).__init__()
         
+    def run(self):
+        self.result={}
+        try:
+            results = self.scrap_ebay()
+            if len(results) == 0:
+                self.result = {}
+                print('Ebay_results empty')
+            else:
+                item=results[0]
+                atag = item.find("a",{"class":"s-item__link"})
+                self.result['description'] = item.find("h3",{"class":"s-item__title"}).get_text().strip()
+                self.result['url'] = atag.get('href')
+                self.result['url'] = shorten_url(self.result['url'])
+                self.result['price'] = item.find("span",{"class":"s-item__price"}).get_text().strip()
+                self.result['site'] = 'ebay'
+        except:
+            print('Ebay_results exception')
+            self.result = {}
+        
+    def get_driver(self):
+        options = webdriver.ChromeOptions()
+        #options.headless = True
+        driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
+        return driver
+    
     def get_url_ebay(self):
       try:
           template="https://www.ebay.com"+"/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw={}"
@@ -26,26 +60,8 @@ class WebScrapper_Ebay():
         try:
             url = self.get_url_ebay()
             self.driver.get(url)
-            print(url)
             soup = BeautifulSoup(self.driver.page_source, 'html.parser')
             results = soup.find_all("li",{"class":"s-item s-item__pl-on-bottom s-item--watch-at-corner"})
         except:
             results = []
         return results
-
-    def extract_item_ebay(self):
-        result={}
-        try:
-            results = self.scrap_ebay()
-            print(results)
-            if len(results) == 0:
-                return result 
-            item=results[0]
-            atag = item.find("a",{"class":"s-item__link"})
-            result['description'] = item.find("h3",{"class":"s-item__title"}).get_text().strip()
-            result['url'] = atag.get('href')
-            result['price'] = item.find("span",{"class":"s-item__price"}).get_text().strip().strip('$')
-            result['site'] = 'ebay'
-        except:
-            result = {}
-        return result
--- code/web_scrappers/WebScrapper_Walmart.py
+++ code/web_scrappers/WebScrapper_Walmart.py
@@ -6,12 +6,47 @@ This code is licensed under MIT license (see LICENSE.MD for details)
 """
 
 from bs4 import BeautifulSoup
+from threading import Thread
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-class WebScrapper_Walmart():
+import sys
+sys.path.append('../')
+from utils.url_shortener import shorten_url
+
+class WebScrapper_Walmart(Thread):
     
-    def __init__(self,driver,description):
-        self.driver = driver
+    def __init__(self,description):
+        self.driver = self.get_driver()
         self.description = description
+        self.result = {}
+        super(WebScrapper_Walmart,self).__init__()
+        
+    def run(self):
+        self.result={}
+        try:
+            results = self.scrap_walmart()
+            if len(results) == 0:
+                self.result = {} 
+                print('Walmart_results empty')
+            else:
+                item=results[0]
+                atag = item.find("a",{"class":"absolute w-100 h-100 z-1"})
+                self.result['description'] = (atag.find("span",{"class":"w_DJ"})).text
+                self.result['url'] = shorten_url(self.result['url'])
+                self.result['url'] = atag.get('href')
+                parent_price= item.find("div",{"class":"flex flex-wrap justify-start items-center lh-title mb2 mb1-m"})
+                self.result['price'] = parent_price.find("div", {"class":"b black f5 mr1 mr2-xl lh-copy f4-l"}).text
+                self.result['site'] = 'walmart'
+        except:
+            print('Walmart_results exception')
+            self.result = {}
+            
+    def get_driver(self):
+        options = webdriver.ChromeOptions()
+        #options.headless = True
+        driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
+        return driver
         
     def get_url_walmart(self):
         template = 'https://www.walmart.com/search?q={}'
@@ -23,19 +58,5 @@ class WebScrapper_Walmart():
         self.driver.get(url)
         soup = BeautifulSoup(self.driver.page_source, 'html.parser')
         results = soup.find_all('div',{'class': 'h-100 pb1-xl pr4-xl pv1 ph1'})
-        print('results:{}'.format(results))
         return results
 
-    def extract_item_walmart(self):
-        result={}
-        results = self.scrap_walmart()
-        if len(results) == 0:
-            return result 
-        item=results[0]
-        atag = item.find("a",{"class":"absolute w-100 h-100 z-1"})
-        result['description'] = (atag.find("span",{"class":"w_DJ"})).text
-        result['url'] = atag.get('href')
-        parent_price= item.find("div",{"class":"flex flex-wrap justify-start items-center lh-title mb2 mb1-m"})
-        result['price'] = parent_price.find("div", {"class":"b black f5 mr1 mr2-xl lh-copy f4-l"}).text.strip('$')
-        result['site'] = 'walmart'
-        return result
