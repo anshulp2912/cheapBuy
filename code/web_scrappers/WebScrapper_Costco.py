a96fc54e73a648bd7a5052b0bb27430f37a05ab6
darshanp228 <72122805+darshanp228@users.noreply.github.com>
2021-11-04 01:18:16 -0400

    Added Multithreading scrapper
    Implemented Multithreading for Bjs and Cotco scrapper

--- code/web_scrappers/WebScrapper_Costco.py
+++ code/web_scrappers/WebScrapper_Costco.py
@@ -6,15 +6,49 @@ This code is licensed under MIT license (see LICENSE.MD for details)
 """
 
 from bs4 import BeautifulSoup
+from threading import Thread
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-class WebScrapper_Costco():
+import sys
+sys.path.append('../')
+from utils.url_shortener import shorten_url
+
+class WebScrapper_Costco(Thread):
     
-    def __init__(self,driver,description):
-        self.driver = driver
+    def __init__(self,description):
+        self.driver = self.get_driver()
         if len(description)<5:
             self.description = description
         else:
             self.description = ' '.join(description.split()[:5])
+        self.result = {}
+        super(WebScrapper_Costco,self).__init__()
+        
+    def run(self):
+        self.result={}
+        try:
+            results = self.scrap_costco()
+            if len(results) == 0:
+                print('Costco_results empty')
+                self.result={}
+            else:                
+                item=results[0]
+                atag = item.find("span",{"class":"description"}).find('a')
+                self.result['description'] = atag.text
+                self.result['url'] = atag.get('href')
+                self.result['url'] = shorten_url(self.result['url'])
+                self.result['price'] = item.find("div",{"class":"price"}).text.strip()
+                self.result['site'] = 'costco'
+        except:
+            print('Costco_results exception')
+            self.result={}
+            
+    def get_driver(self):
+        options = webdriver.ChromeOptions()
+        #options.headless = True
+        driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
+        return driver
     
     def get_url_costco(self):
         template = "https://www.costco.com"+"/CatalogSearch?dept=All&keyword={}"
@@ -27,17 +61,5 @@ class WebScrapper_Costco():
         soup = BeautifulSoup(self.driver.page_source,"html.parser")
         results = soup.find_all('div',{'class': 'product-list grid'})
         return results
-
-    def extract_item_costco(self):
-        result={}
-        results = self.scrap_costco()
-        if len(results) == 0:
-            print(result) 
-        item=results[0]
-        atag = item.find("span",{"class":"description"}).find('a')
-        result['description'] = atag.text
-        result['url'] = atag.get('href')
-        result['price'] = item.find("div",{"class":"price"}).text.strip()
-        result['site'] = 'costco'
-        return result
+        
 
