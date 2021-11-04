bab36692f97bef55e49ff69434a26471d5bd8f41
rohan22shah <42119767+rohan22shah@users.noreply.github.com>
2021-11-04 01:16:13 -0400

    Added Multithreading
    Implemented Multithreading to improve performance.

--- code/web_scrappers/WebScrapper.py
+++ code/web_scrappers/WebScrapper.py
@@ -4,17 +4,15 @@ This code is licensed under MIT license (see LICENSE.MD for details)
 
 @author: cheapBuy
 """
-import os
-import sys
-sys.path.append(os.path.abspath('../../../'))
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from web_scrappers.FetchDescription import FetchDescription
-from web_scrappers.WebScrapper_Amazon import WebScrapper_Amazon
-from web_scrappers.WebScrapper_Bjs import WebScrapper_Bjs
-from web_scrappers.WebScrapper_Ebay import WebScrapper_Ebay
-from web_scrappers.WebScrapper_Costco import WebScrapper_Costco
-from web_scrappers.WebScrapper_Walmart import WebScrapper_Walmart
+from FetchDescription import FetchDescription
+from WebScrapper_Amazon import WebScrapper_Amazon
+from WebScrapper_Bjs import WebScrapper_Bjs
+from WebScrapper_Ebay import WebScrapper_Ebay
+from WebScrapper_Costco import WebScrapper_Costco
+from WebScrapper_Walmart import WebScrapper_Walmart
 
 class WebScrapper:
     
@@ -23,7 +21,6 @@ class WebScrapper:
     
     def get_driver(self):
         options = webdriver.ChromeOptions()
-        #options.headless = True
         driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
         return driver
 
@@ -56,16 +53,34 @@ class WebScrapper:
     def call_scrapper(self):
         product_description = self.get_description()
         print(product_description)
-        driver = self.get_driver()
         
-        results_amazon = WebScrapper_Amazon(driver,product_description).extract_item_amazon()
-        results_walmart = WebScrapper_Walmart(driver,product_description).extract_item_walmart()
-        results_ebay = WebScrapper_Ebay(driver,product_description).extract_item_ebay()
-        results_bjs = WebScrapper_Bjs(driver,product_description).extract_item_bjs()
-        results_costco = WebScrapper_Costco(driver,product_description).extract_item_costco()
+        t_amazon = WebScrapper_Amazon(product_description)
+        t_walmart = WebScrapper_Walmart(product_description)
+        t_ebay = WebScrapper_Ebay(product_description)
+        t_bjs = WebScrapper_Bjs(product_description)
+        t_costco = WebScrapper_Costco(product_description)
+        
+        t_amazon.start()
+        t_walmart.start()
+        t_ebay.start()
+        t_bjs.start()
+        t_costco.start()
+        
+        t_amazon.join()
+        t_walmart.join()
+        t_ebay.join()
+        t_bjs.join()
+        t_costco.join()
+        
+        results_amazon = t_amazon.result
+        results_walmart = t_walmart.result
+        results_ebay = t_ebay.result
+        results_bjs = t_bjs.result
+        results_costco = t_costco.result
+        
+        return [results_amazon, results_walmart, results_ebay, results_bjs, results_costco]
 
-        return [results_amazon,results_walmart,results_ebay,results_bjs,results_costco]
-
+#link = 'https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038'
 #link = 'https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038'
 #ws = WebScrapper(link)
 #result=ws.call_scrapper()        
--- code/web_scrappers/WebScrapper_Amazon.py
+++ code/web_scrappers/WebScrapper_Amazon.py
@@ -6,48 +6,65 @@ This code is licensed under MIT license (see LICENSE.MD for details)
 """
 
 from bs4 import BeautifulSoup
+from threading import Thread
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-class WebScrapper_Amazon():
+import sys
+sys.path.append('../')
+from utils.url_shortener import shorten_url
+
+class WebScrapper_Amazon(Thread):
     
-    def __init__(self,driver,description):
-        self.driver = driver
+    def __init__(self,description):
+        self.driver = self.get_driver()
         self.description = description
+        self.result = {}
+        super(WebScrapper_Amazon,self).__init__()
+    
+    def run(self):
+        try:
+            results = self.scrap_amazon()
+            if len(results) == 0:
+                print('Amazon_results empty')
+                self.result = {}
+            else:
+                item=results[0]
+                atag = item.h2.a
+                self.result['description'] = atag.text.strip()
+                self.result['url'] = 'https://www.amazon.com'+atag.get('href')
+                self.result['url'] = shorten_url(self.result['url'])
+                price_parent = item.find('span', 'a-price')
+                self.result['price'] = price_parent.find('span', 'a-offscreen').text
+                self.result['site'] = 'amazon'
+        except:
+            print('Amazon_results exception')
+            self.result={}
+
+    def get_driver(self):
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
+        return driver
     
     def get_url_amazon(self):
-    	try:
-    		template = 'https://www.amazon.com'+'/s?k={}&ref=nb_sb_ss_ts-doa-p_3_5'
-    		search_term = self.description.replace(' ','+')
-    		template = template.format(search_term)
-    	except:
-    		template = ''
-    	return template
+        try:
+            template = 'https://www.amazon.com'+'/s?k={}&ref=nb_sb_ss_ts-doa-p_3_5'
+            search_term = self.description.replace(' ','+')
+            template = template.format(search_term)
+        except:
+            template = ''
+        return template
 
     def scrap_amazon(self):
-    	results=[]
-    	try:
-    		url = self.get_url_amazon()
-    		self.driver.get(url)
-    		soup = BeautifulSoup(self.driver.page_source, 'html.parser')
-    		results = soup.find_all('div',{'data-component-type': 's-search-result'})
-    	except:
-    		results = []
-    	return results
+        results=[]
+        try:
+            url = self.get_url_amazon()
+            self.driver.get(url)
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+            results = soup.find_all('div',{'data-component-type': 's-search-result'})
+        except:
+            results = []
+        return results
 
 
-    def extract_item_amazon(self):
-    	result={}
-    	try:
-    		results = self.scrap_amazon()
-    		if len(results) == 0:
-    			return result
-    		item=results[0]
-    		atag = item.h2.a
-    		result['description'] = atag.text.strip()
-    		result['url'] = 'https://www.amazon.com'+atag.get('href')
-    		price_parent = item.find('span', 'a-price')
-    		result['price'] = price_parent.find('span', 'a-offscreen').text
-    		result['site'] = 'amazon'
-    	except:
-    		result={}
-    	return result
 
