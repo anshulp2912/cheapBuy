c2639c82d7bf5a44786d14a930893a366af5b649
rohan22shah <42119767+rohan22shah@users.noreply.github.com>
2021-11-04 12:45:44 +0530

    Update WebScrapper_Amazon.py

--- source/web_scrappers/WebScrapper_Amazon.py
+++ source/web_scrappers/WebScrapper_Amazon.py
@@ -44,7 +44,7 @@ class WebScrapper_Amazon(Thread):
     def get_driver(self):
         options = webdriver.ChromeOptions()
         options.add_argument('--no-sandbox')
-        driver = webdriver.Chrome(chrome_options=options, executable_path=ChromeDriverManager().install())
+        driver = webdriver.Chrome(chrome_options=options, executable_path=os.path.abspath('chromedriver'))
         return driver
     
     def get_url_amazon(self):
