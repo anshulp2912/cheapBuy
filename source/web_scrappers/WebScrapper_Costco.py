88b00421295aa669305ab6036bc020eec6a70e5e
Anshul Patel <36321866+anshulp2912@users.noreply.github.com>
2021-11-04 02:13:05 -0400

    Removed utils from path

--- source/web_scrappers/WebScrapper_Costco.py
+++ source/web_scrappers/WebScrapper_Costco.py
@@ -12,7 +12,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import sys
 sys.path.append('../')
-from source.utils.url_shortener import shorten_url
+from utils.url_shortener import shorten_url
 
 class WebScrapper_Costco(Thread):
     
