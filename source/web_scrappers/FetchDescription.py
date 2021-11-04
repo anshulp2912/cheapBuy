d8d1a2a774a428dcd4ec8a1a82cfb74b81210321
Darshan Patel <72122805+darshanp228@users.noreply.github.com>
2021-11-04 04:15:00 -0400

    Update FetchDescription.py

--- source/web_scrappers/FetchDescription.py
+++ source/web_scrappers/FetchDescription.py
@@ -43,8 +43,7 @@ class FetchDescription():
     
     def fetch_desc_ebay(self):
         try:
-            headers = {"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
-            html =  requests.get(self.product_link,headers=headers)
+            html =  requests.get(self.product_link)
             soup=BeautifulSoup(html.content,'html.parser')
             product = soup.find("div",{"class" : "vi-swc-lsp"}, { "id" : "itemTitle" })
             title = product.find('span', class_="u-dspn").text.strip()
