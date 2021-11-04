3d17fde92b4500bd7c03238df7910e641417bebf
Darshan Patel <72122805+darshanp228@users.noreply.github.com>
2021-11-04 03:35:17 -0400

    Update WebScrapper_Amazon.py

--- source/web_scrappers/WebScrapper_Amazon.py
+++ source/web_scrappers/WebScrapper_Amazon.py
@@ -9,7 +9,6 @@ from bs4 import BeautifulSoup
 from threading import Thread
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
 
 import sys
 sys.path.append('../')
@@ -43,9 +42,9 @@ class WebScrapper_Amazon(Thread):
             self.result={}
 
     def get_driver(self):
-        #options = webdriver.ChromeOptions()
-        #options.add_argument('--no-sandbox')
-        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
         return driver
     
     def get_url_amazon(self):
