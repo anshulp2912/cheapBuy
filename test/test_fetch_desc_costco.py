4d1020bbf806e238cb8a80541b492935b2dd0e1c
Darshan Patel <72122805+darshanp228@users.noreply.github.com>
2021-11-04 01:30:19 -0400

    Update test_fetch_desc_costco.py

--- test/test_fetch_desc_costco.py
+++ test/test_fetch_desc_costco.py
@@ -7,7 +7,7 @@ Created on Wed Nov  3 23:52:22 2021
 
 import sys
 sys.path.append('../')
-from code.web_scrappers.FetchDescription import FetchDescription
+from source.web_scrappers.FetchDescription import FetchDescription
 
 def test_fetch_description_costco():
     link = "https://www.costco.com/brita-replacement-filters%2c-10-pack.product.100131571.html"
