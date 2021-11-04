2836c4dc3372ebc87b04160422b33d54616b8aaa
Darshan Patel <72122805+darshanp228@users.noreply.github.com>
2021-11-04 01:31:11 -0400

    Update test_fetch_desc_ebay.py

--- test/test_fetch_desc_ebay.py
+++ test/test_fetch_desc_ebay.py
@@ -7,7 +7,7 @@ Created on Wed Nov  3 23:52:22 2021
 
 import sys
 sys.path.append('../')
-from code.web_scrappers.FetchDescription import FetchDescription
+from source.web_scrappers.FetchDescription import FetchDescription
 
 def test_fetch_description_ebay():
     link = "https://www.ebay.com/itm/274922036305?epid=25014370331&hash=item4002a15c51:g:v8UAAOSwsv1hKB7z"
