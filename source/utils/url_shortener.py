7725263bf16d27f94b84505a913d3798c21b7a9b
pragna98 <46415182+pragna98@users.noreply.github.com>
2021-11-04 11:49:01 -0400

    Update url_shortener.py

--- source/utils/url_shortener.py
+++ source/utils/url_shortener.py
@@ -5,9 +5,11 @@ This code is licensed under MIT license (see LICENSE.MD for details)
 @author: cheapBuy
 """
 
+# Import Libraries
 from pyshorteners import Shortener 
 
 def shorten_url(url):
+    # Shorten the passed url
     s = Shortener()
     short_url = s.tinyurl.short(url)
     return short_url
